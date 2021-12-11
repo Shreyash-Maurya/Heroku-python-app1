1b8a91ac1e8083893c286996b379d1890933b987
Shreyash-Maurya <shreyashmaurya2002@gmail.com>
2021-12-11 13:01:29 +0530

    first commit

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
-"""#from flask import Flask, render_template
+from flask import Flask, render_template
 import schedule
-#import datetime
-#app = Flask(__name__)
-
-#@app.route('/')
-#def hello_world():
-#    return render_template("index.html")
-#    #return 'Hello World !'
-
-#if __name__ == "__main__":
-#    app.run(debug=True)
-
-def job():
-    print("Hello World")
-    
-schedule.every(10).seconds.do(job)
-while True:
-    schedule.run_pending()
-"""
-from os import environ
-from flask import Flask
-
 app = Flask(__name__)
-app.run(environ.get('PORT'))
 
-print("Hello world" * 100)
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+    #return 'Hello World !'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
