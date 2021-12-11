86e27be4112476d3f67216b964ac4cd00603e453
Shreyash-Maurya <shreyashmaurya2002@gmail.com>
2021-12-11 10:20:20 +0530

    second commit

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+#from flask import Flask, render_template
+import schedule
+#import datetime
+#app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
-    #return 'Hello World !'
+#@app.route('/')
+#def hello_world():
+#    return render_template("index.html")
+#    #return 'Hello World !'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
+def job():
+    print("Hello World")
     
-    
+schedule.every(10).seconds.do(job)
+while True:
+    schedule.run_pending()
